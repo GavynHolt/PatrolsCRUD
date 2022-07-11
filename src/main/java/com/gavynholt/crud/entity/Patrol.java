3e7f9da9e88759d5282337d0e1fc8948657c90d4
Gavyn Holt <gavynholt@gmail.com>
2022-07-11 09:43:43 -0400

    fix: getter setter for id

--- src/main/java/com/gavynholt/crud/entity/Patrol.java
+++ src/main/java/com/gavynholt/crud/entity/Patrol.java
@@ -25,6 +25,14 @@ public class Patrol {
         this.notes = notes;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
