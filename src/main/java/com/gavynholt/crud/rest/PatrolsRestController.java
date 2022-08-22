83789428a2fd5a32f2d4e8773536d241b8ec183a
Gavyn Holt <gavynholt@gmail.com>
2022-08-21 21:04:02 -0400

    fix: remove https

--- src/main/java/com/gavynholt/crud/rest/PatrolsRestController.java
+++ src/main/java/com/gavynholt/crud/rest/PatrolsRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://https://localhost:4200")
+@CrossOrigin(origins = "https://http://localhost:4200")
 @RequestMapping("api")
 public class PatrolsRestController {
 
