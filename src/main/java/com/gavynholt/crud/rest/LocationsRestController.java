81ae4c2c0961d6ea26f980f129cf7ee3c3341907
Gavyn Holt <gavynholt@gmail.com>
2022-08-21 21:26:35 -0400

    fix: add more CORS

--- src/main/java/com/gavynholt/crud/rest/LocationsRestController.java
+++ src/main/java/com/gavynholt/crud/rest/LocationsRestController.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "https://patrol.staging.rspndr.io",
+})
 @RequestMapping("api")
 public class LocationsRestController {
 
