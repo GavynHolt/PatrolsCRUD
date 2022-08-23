5f4c6a95519ec365f6f8d80e03ac435e8f8ce482
Gavyn Holt <gavynholt@gmail.com>
2022-08-23 11:32:55 -0400

    fix: add getters and setters

--- src/main/java/com/gavynholt/crud/entity/Step.java
+++ src/main/java/com/gavynholt/crud/entity/Step.java
@@ -26,4 +26,56 @@ public class Step {
     @ManyToOne(cascade={CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name="post_order_id")
     private PostOrder postOrder;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getPhotoRequired() {
+        return photoRequired;
+    }
+
+    public void setPhotoRequired(Boolean photoRequired) {
+        this.photoRequired = photoRequired;
+    }
+
+    public String getEscalationContact() {
+        return escalationContact;
+    }
+
+    public void setEscalationContact(String escalationContact) {
+        this.escalationContact = escalationContact;
+    }
+
+    public PostOrder getPostOrder() {
+        return postOrder;
+    }
+
+    public void setPostOrder(PostOrder postOrder) {
+        this.postOrder = postOrder;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Step{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", photoRequired=").append(photoRequired);
+        sb.append(", escalationContact='").append(escalationContact).append('\'');
+        sb.append(", postOrder=").append(postOrder);
+        sb.append('}');
+        return sb.toString();
+    }
 }
